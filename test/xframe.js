4ab8eaf15ed03e5a099eb681560d2bcdcadc632b
fengmk2 <fengmk2@gmail.com>
2014-06-09 18:33:44 +0800

    Xframes support enable detect options

--- test/xframe.js
+++ test/xframe.js
@@ -5,6 +5,7 @@
 var lusca = require('../index'),
     request = require('supertest'),
     assert = require('assert'),
+    pedding = require('pedding'),
     mock = require('./mocks/app');
 
 
@@ -44,4 +45,40 @@ describe('XFRAME', function () {
             .expect(200, done);
     });
 
+    it('header (sameorigin) with options.enable true', function (done) {
+        var enable = function (url) {
+            return url.indexOf('/show') >= 0;
+        };
+        var config = { xframe: { value: 'SAMEORIGIN', enable: enable } },
+            app = mock(config);
+
+        app.get('/show', function (req, res) {
+            res.send(200);
+        });
+
+        request(app)
+            .get('/show')
+            .expect('X-FRAME-OPTIONS', config.xframe.value)
+            .expect(200, done);
+    });
+
+    it('header (sameorigin) with options.enable false', function (done) {
+        var enable = function (url) {
+            return url.indexOf('/show') >= 0;
+        };
+        var config = { xframe: { value: 'SAMEORIGIN', enable: enable } },
+            app = mock(config);
+
+        app.get('/', function (req, res) {
+            res.send(200);
+        });
+
+        request(app)
+            .get('/')
+            .expect(200, function (err, res) {
+                assert(!err);
+                assert(res.headers['x-frame-options'] === undefined);
+                done();
+            });
+    });
 });
