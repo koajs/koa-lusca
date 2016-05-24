0cf397016f59bfae04caf3b96a2da80f76ee24a1
Marek Fajkus <marek.faj@gmail.com>
2016-05-24 11:43:19 +0200

    Feature/improve csp support (#11)
    * Add suport for String and Array CSP policy rules
    
    * Update CSP documentation
    
    * Define full CSP API requirements in tests
    
    Will break build!
    Please see https://github.com/krakenjs/lusca/pull/79 for reference
    
    * implement new CSP parsing
    
    * Update CSP documentation in README
    
    * Depricate node 0.11, add node 4, 5, 6
    
    * update travis.yml node versions

--- test/csp.js
+++ test/csp.js
@@ -37,8 +37,61 @@ describe('CSP', function () {
 
     request(app.listen())
     .get('/')
-    .expect('Content-Security-Policy', 'default-src *; ')
+    .expect('Content-Security-Policy', 'default-src *')
     .expect('hello')
+    .expect(200, done);
+  });
+
+  it('string config', function (done) {
+    var app = mock({
+      csp: {
+        policy: 'default-src *'
+      }
+    });
+
+    app.get('/', function (req, res) {
+      res.status(200).end();
+    });
+
+    request(app.listen())
+    .get('/')
+    .expect('Content-Security-Policy', 'default-src *')
+    .expect(200, done);
+  });
+
+  it('array config', function (done) {
+    var app = mock({
+      csp: {
+        policy: ['default-src *', 'img-src *']
+      }
+    });
+
+    app.get('/', function* () {
+      this.body = 'hello';
+    });
+
+    request(app.listen())
+    .get('/')
+    .expect('Content-Security-Policy', 'default-src *; img-src *')
+    .expect(200, done);
+  });
+
+  it('nested config', function (done) {
+    var app = mock({
+      csp: {
+        policy: [
+        { 'default-src': '*' },
+        'img-src *'
+        ]
+      }
+    });
+    app.get('/', function* () {
+      this.body = 'hello';
+    });
+
+    request(app.listen())
+    .get('/')
+    .expect('Content-Security-Policy', 'default-src *; img-src *')
     .expect(200, done);
   });
 });
